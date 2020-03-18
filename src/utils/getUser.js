529a7cb849aae3994628ec2f5697050e0d83bc75
Damián De Lio <damiandelio@gmail.com>
2020-03-18 13:45:44 -0300

    small syntax fixes

--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -2,16 +2,17 @@ import { verify } from "jsonwebtoken";
 import { decryptStr } from "./encryption";
 
 const getUser = encryptedToken => {
-  let user;
-  // secret key
-  const key = process.env.ACCESS_TOKEN_SECRET;
+  let payload, user;
+  const key = process.env.ACCESS_TOKEN_SECRET; // secret key
 
   // if a token is defined in the header
   if (encryptedToken) {
     // decrypt token
     const token = decryptStr(encryptedToken);
-    // try to retrieve a user id with the token
-    user = verify(token, key).user;
+    // try to retrieve the payload from the token
+    payload = verify(token, key);
+    // get user id
+    user = payload.user;
   }
 
   return user;
